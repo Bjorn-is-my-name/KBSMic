6bdafbce42c672a058c4615d46b41b668a2f1acd
ekkeXP <eekeljan@gmail.com>
2022-11-18 10:43:43 +0100

    map configuratie AVR aangemaakt

---
+++ build/CMakeFiles/GIT.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  ".lst"
+  "CMakeFiles/GIT.dir/src/main.cpp.obj"
+  "CMakeFiles/GIT.dir/src/main.cpp.obj.d"
+  "GIT.eeprom"
+  "GIT.elf"
+  "GIT.elf.manifest"
+  "GIT.elf.pdb"
+  "GIT.hex"
+  "GIT.lss"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GIT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/disasm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  ".lst"
+  "CMakeFiles/disasm"
+  "GIT.eeprom"
+  "GIT.hex"
+  "GIT.lss"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/disasm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

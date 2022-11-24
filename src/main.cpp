50b54aae0270f9541bab8bb92828da57b6adb292
ekkeXP <eekeljan@gmail.com>
2022-11-24 11:05:15 +0100

    yoinked van bjorn

--- src/main.cpp
+++ src/main.cpp
@@ -4,102 +4,161 @@
 #include <Wire.h>
 #include <Adafruit_ILI9341.h>
 #include <Nunchuk.h>
-#include "HardwareSerial.h"
-void init_IR();
+
 void init_timer0();
 void init_timer2();
+void sendData();
 
 #define NUNCHUK_ADDRESS 0x52
+#define IR_38KHZ 52
+#define IR_52KHZ 37
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 240
+#define PLAYER_WIDTH 20
+#define PLAYER_HEIGHT 20
+#define OFF_TIME 70
+#define ZERO_TIME 70
+#define ONE_TIME 210
 
 #define TFT_CS 10
 #define TFT_DC 9
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
-ISR(TIMER0_COMPA_vect)
+// Check to see if the current bit is done sending
+bool dataIsSend = false;
+// Data to send over IR
+uint8_t sendingData = 0b10011001;
+// The data bit to send;
+uint8_t sendingBit = 0;
+
+// Toggle IR light
+ISR(TIMER2_COMPB_vect)
 {
-    //IR-LED gaat hier aan en uit. geen code nodig.
+    // If the bit is done sending, wait before sending the next bit
+    if (dataIsSend)
+    {
+        // Time after which to continue to the next bit
+        OCR2A = OFF_TIME;
+        // Disable TC0
+        TCCR0A |= (1 << COM0A1);
+    }
+    // If the waiting time is passed, send the next bit
+    else
+    {
+        if (sendingBit < 8)
+        {
+            // Set the time corresponding to the bit
+            OCR2A = ((sendingData >> sendingBit++) & 1) ? ONE_TIME : ZERO_TIME;
+            // Enable TC0
+            TCCR0A &= ~(1 << COM0A1);
+        }
+        else
+        {
+            // Once all bits are send, disable TC2 by removing the clock source
+            TCCR2B &= ~((1 << CS22) | (1 << CS20));
+            // Reset the sending bit for next run
+            sendingBit = 0;
+        }
+    }
 
+    // Flip between sending a bit and waiting
+    dataIsSend = !dataIsSend;
 }
-
-ISR(TIMER2_COMPA_vect)
-{
-    //Toggle iets
-    TCCR0A ^= (1 << COM0A1);
-}
-
 
 int main(void) {
-    uint16_t pos[] = {SCREEN_WIDTH/2, SCREEN_HEIGHT/2};
+    // Player position (x and y flipped because they are flipped on the joystick)
+    uint16_t pos[] = {SCREEN_HEIGHT / 2, SCREEN_WIDTH / 2};
 
-    init_IR();
+    // Setup IR
+    DDRD |= (1 << DDD6);
     init_timer0();
     init_timer2();
 
+    // Setup screen
     sei();
     Wire.begin();
-
     tft.begin();
     tft.fillScreen(ILI9341_BLACK);
 
+    // Check nunckuk connection
     if (!Nunchuk.begin(NUNCHUK_ADDRESS))
-        return -1;
+        tft.fillScreen(ILI9341_RED);
 
     while (1) {
+        // Clear old position
+        tft.fillRect(pos[0], pos[1], PLAYER_WIDTH, PLAYER_HEIGHT, ILI9341_BLACK);
+
+        // Position change lambda function
         [&pos]()
         {
-            if(!Nunchuk.getState(NUNCHUK_ADDRESS)){
+            // Check nunchuk connection
+            if(!Nunchuk.getState(NUNCHUK_ADDRESS))
                 return 0;
+
+            // Check for joystick usage
+
+            // Right
+            if (Nunchuk.state.joy_x_axis > 140) {
+                if (pos[1] + PLAYER_WIDTH < SCREEN_WIDTH)
+                    pos[1]++;
+            }
+            // Left
+            else if (Nunchuk.state.joy_x_axis < 100) {
+                if (pos[1] > 1)
+                    pos[1]--;
             }
 
-            if (Nunchuk.state.joy_x_axis > 140 && pos[1] <= SCREEN_WIDTH-20) {
-                pos[1]++;
+            // Upwards acceleration
+            if (Nunchuk.state.accel_z_axis <= 10 && pos[0] <= SCREEN_HEIGHT - PLAYER_HEIGHT) {
+                pos[0]+= 5;
             }
-            if (Nunchuk.state.joy_x_axis < 100 && pos[1] >= 1) {
-                pos[1]--;
-            }
-
-            if (Nunchuk.state.accel_z_axis <= 10 && pos[0] <= SCREEN_HEIGHT-20) {
-                pos[0]+=5;
-            }else if(pos[0] >= 1){
+            else if(pos[0] >= 1){
                 pos[0]--;
             }
-//             if (Nunchuk.state.joy_y_axis < 100 && pos[1] >= 0) {
-//                 pos[1]--;
-//             }
         }();
-        int16_t W = 20;
-        int16_t H = 20;
-        // Compare      Y       X       +Y   +X
-        tft.fillRect(pos[0]-1, pos[1]-1, H+2, W+2, ILI9341_BLACK);
-        tft.fillRect(pos[0], pos[1], W, H, ILI9341_WHITE);
+
+        // Draw new position
+        tft.fillRect(pos[0], pos[1], PLAYER_WIDTH, PLAYER_HEIGHT, ILI9341_WHITE);
+
+        // Send the data over IR
+        sendData();
     }
 
     return (0);
 }
 
-void init_IR()
+void init_timer0() 
 {
-    DDRD |= (1 << PD6);                         //Pin D6 as output (IR-LED)
-}
+    /*
+    Fast-PWM mode (TOP = OCRA) -> WGM0[2:0] = 0b111
+    Set on compare match and clear on bottom (effectively pulling OC0A low) -> COM0A[1:0] = 0b11
+    Prescale /8 -> CS0[2:0] = 0b010
+    */
+    TCCR0A |= (1 << COM0A0) | (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);
+    TCCR0B |= (1 << WGM02) | (1 << CS01);
 
-void init_timer0()
-{
-    TCCR0A |= (1 << WGM00) | (1 << WGM01);      //fast-PWM + CTC
-    TCCR0B |= (1 << WGM02);                     //^^^^^^^^^^^^^^
-    TCCR0B |= (1 << CS01);                      //prescaler 8
-    OCR0A = 52;                                 //compare value = 52 = 38kHZ  35 = 56kHZ
-    TIMSK0 |= (1 << OCIE0A);                    //enable interrupts
-    TCCR0A |= (1 << COM0A0);                    //enables toggle on OC0A
+    // Compare value
+    OCR0A = IR_38KHZ;
 }
 
 void init_timer2()
 {
-    TCCR2A |= (1 << WGM21);                     //CTC
-    TCCR2B |= (1 << CS22);                      //64 prescale
-    //1 is aan voor x us
-    //0 is aan voor x us
-    OCR2A = 200;                                //Comp waarde = 100
-    TIMSK2 |= (1 << OCIE2A);                    //Enable interupts
+    // CTC mode
+    TCCR2A |= (1 << WGM21);
+
+    // Enable interupts on compare match
+    TIMSK2 |= (1 << OCIE2B);
+}
+
+void sendData()
+{
+    // Return if already sending data
+    if((TCCR2B >> CS20) & 1)
+        return;
+
+    // Force an interupt to start the sending
+    OCR2A = 1;
+
+    // Enable TC2 by setting the prescaler (set to /128)
+    TCCR2B |= (1 << CS22) | (1 << CS20); 
 }

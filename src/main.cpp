845db0492004a37584e7c329a27a5382e77007a5
Bjorn <72093834+Bjorn-is-my-name@users.noreply.github.com>
2022-12-15 14:48:00 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,43 @@
 #include "Background.c"
 #include "Nunchuk.cpp"
 
+struct
+{
+public:
+    uint16_t x = 14;
+    uint8_t y = 170;
+    uint16_t xOld = x;
+    uint8_t yOld = y;
+    int8_t yVelocity = 0;
+    bool jumping = false;
+}player1;
+
+struct
+{
+public:
+    uint16_t x = 13;
+    uint8_t y = 210;
+    uint16_t xOld = x;
+    uint8_t yOld = y;
+    // uint8_t animation
+} player2;
+
+struct Rect
+{
+    uint16_t x;
+    uint8_t y;
+    uint8_t width;
+    uint8_t height;
+};
+
+struct Collect
+{
+    uint16_t x;
+    uint8_t y;
+    uint8_t w;
+    uint8_t h;
+};
+
 void initTimer0();
 
 void checkWallCollision();
@@ -33,7 +70,7 @@ void clearSprite(uint16_t, uint8_t, uint16_t, uint8_t, uint8_t, uint8_t, const u
 
 bool pointInRect(uint16_t, uint8_t, uint16_t, uint8_t, uint8_t, uint8_t);
 
-bool rectangleCollision(uint16_t playerX, uint8_t playerY, Rect wall)
+bool rectangleCollision(uint16_t playerX, uint8_t playerY, Rect wall);
 
 void drawSprite(uint16_t, uint8_t, uint8_t, uint8_t, const uint8_t *, uint8_t ver = 0);
 
@@ -99,43 +136,6 @@ void draw();
 #define LEVER_TOP_WIDTH 4
 #define LEVER_TOP_HEIGHT 7
 
-struct
-{
-public:
-    uint16_t x = 14;
-    uint8_t y = 170;
-    uint16_t xOld = x;
-    uint8_t yOld = y;
-    int8_t yVelocity = 0;
-    bool jumping = false;
-} player1;
-
-struct
-{
-public:
-    uint16_t x = 13;
-    uint8_t y = 210;
-    uint16_t xOld = x;
-    uint8_t yOld = y;
-    // uint8_t animation
-} player2;
-
-struct Rect
-{
-    uint16_t x;
-    uint8_t y;
-    uint8_t width;
-    uint8_t height;
-};
-
-struct Collect
-{
-    uint16_t x;
-    uint8_t y;
-    uint8_t w;
-    uint8_t h;
-};
-
 Rect walls[] = {
     Rect{0, 0, 5, 240},
     Rect{10, 0, 155, 10},
@@ -152,13 +152,15 @@ Rect walls[] = {
     Rect{150, 150, 5, 30},
     Rect{150, 180, 52, 10},
     Rect{280, 210, 15, 20},
-    Rect{10, 190, 40, 10}};
+    Rect{10, 190, 40, 10}
+};
 
 Collect Dias[] = {
     Collect{12, 12, DIA_WIDTH * 2, DIA_HEIGHT},
     Collect{164, 216, DIA_WIDTH * 2, DIA_HEIGHT},
     Collect{26, 12, DIA_WIDTH * 2, DIA_HEIGHT},
-    Collect{229, 216, DIA_WIDTH * 2, DIA_HEIGHT}};
+    Collect{229, 216, DIA_WIDTH * 2, DIA_HEIGHT}
+};
 
 Rect Door1 = {55, 40, DOOR_WIDTH * 4, DOOR_HEIGHT * 3};
 Rect Door2 = {80, 40, DOOR_WIDTH * 4, DOOR_HEIGHT * 3};
@@ -190,7 +192,7 @@ enum gameState
     MENU,
     GAME,
     LEVELSELECT
-} currentGameState = GAME;
+} currentGameState;
 
 ISR(PCINT2_vect)
 {
@@ -285,7 +287,7 @@ int main(void)
 {
     // Setup IR sending
     DDRD |= (1 << DDD6);
-    init_timer0();
+    initTimer0();
 
     // Setup IR recieving
     PORTD |= (1 << PORTD2); // pull up
@@ -293,6 +295,7 @@ int main(void)
     PCMSK2 |= (1 << PCINT18);
 
     setFreq(IR_38KHZ);
+    currentGameState = GAME;
 
     // Setup screen
     Wire.begin();
@@ -471,7 +474,7 @@ bool pointInRect(uint16_t pointX, uint8_t pointY, uint16_t x, uint8_t y, uint8_t
 
 bool rectangleCollision(uint16_t playerX, uint8_t playerY, Rect wall)
 {
-    playerX + PLAYER_ACTUAL_WIDTH > wall.x && playerX < wall.x + (wall.width * 2) && playerY + PLAYER_HEIGHT > wall.y && playerY < wall.y + wall.height
+    return playerX + PLAYER_ACTUAL_WIDTH > wall.x && playerX < wall.x + (wall.width * 2) && playerY + PLAYER_HEIGHT > wall.y && playerY < wall.y + wall.height;
 }
 
 void drawSprite(uint16_t x, uint8_t y, uint8_t w, uint8_t h, const uint8_t *Sprite, uint8_t ver)
